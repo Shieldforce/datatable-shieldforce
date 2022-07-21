f58c5530b6a49c2bbac5e61a50f35277d988be81
Alexandre Ferreira <shieldforce2@gmail.com>
2022-07-20 23:46:59 -0300

    adicionando heads

--- src/Frontend/CssRender/Ghost.php
+++ src/Frontend/CssRender/Ghost.php
@@ -7,9 +7,8 @@ class Ghost
     public static function head()
     {
         return "
-            <link href='../../assets/plugins/datatables.net-bs5/css/dataTables.bootstrap5.min.css' rel='stylesheet' />
-            <link href='../../assets/plugins/datatables.net-responsive-bs5/css/responsive.bootstrap5.min.css' rel='stylesheet' />
-            <link href='../../assets/plugins/bootstrap-table/dist/bootstrap-table.min.css' rel='stylesheet' />
+            <link href='https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.1.3/css/bootstrap.min.css' rel='stylesheet' />
+            <link href='https://cdn.datatables.net/1.12.1/css/dataTables.bootstrap5.min.css' rel='stylesheet' />
         ";
     }
 }
