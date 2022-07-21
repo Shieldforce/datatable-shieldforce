72724c51fa17de8b5354a34994598fcce967cec0
Alexandre Ferreira <shieldforce2@gmail.com>
2022-07-21 01:57:30 -0300

    Criando classe de retorno de dados

--- src/Frontend/CssRender/Ghost.php
+++ src/Frontend/CssRender/Ghost.php
@@ -6,11 +6,6 @@ class Ghost
 {
     public static function head(array $links = [])
     {
-        $array = [
-            "<link href='https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.1.3/css/bootstrap.min.css' rel='stylesheet' />",
-            "<link href='https://cdn.datatables.net/1.12.1/css/dataTables.bootstrap5.min.css' rel='stylesheet' />",
-            "<link href='https://cdn.datatables.net/responsive/2.3.0/css/responsive.dataTables.min.css' rel='stylesheet' />",
-        ];
-        return implode("\n", $array);
+        return implode("\n", $links);
     }
 }
--- src/Frontend/JsRender/Ghost.php
+++ src/Frontend/JsRender/Ghost.php
@@ -4,13 +4,8 @@ namespace Shieldforce\Frontend\JsRender;
 
 class Ghost
 {
-    public static function js()
+    public static function js(array $links = [])
     {
-        return "
-            <script src='https://code.jquery.com/jquery-3.5.1.js'></script>
-            <script src='https://cdn.datatables.net/1.12.1/js/jquery.dataTables.min.js'></script>
-            <script src='https://cdn.datatables.net/1.12.1/js/dataTables.bootstrap5.min.js'></script>
-            <script src='https://cdn.datatables.net/responsive/2.3.0/js/responsive.bootstrap.min.js'></script>
-        ";
+        return implode("\n", $links);
     }
 }
