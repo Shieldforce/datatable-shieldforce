390379740328cd4b5caac84aad8842a9b3e3cf5e
Alexandre Ferreira <shieldforce2@gmail.com>
2022-07-21 01:21:03 -0300

    Criando classe de retorno de dados

--- src/Frontend/JsRender/GhostExecution.php
+++ src/Frontend/JsRender/GhostExecution.php
@@ -6,7 +6,7 @@ use Shieldforce\Backend\DatatableRender;
 
 class GhostExecution
 {
-    public static function js(string $url)
+    public static function js(string $url, string $lengthMenu = '10,20,30,40,50')
     {
         $columns = json_encode(DatatableRender::getColumns());
         $dom     = '<"row mb-3"<"col-sm-4"l><"col-sm-8 text-end"<"d-flex justify-content-end"fB>>>t<"d-flex align-items-center"<"me-auto"i><"mb-0"p>>';
@@ -16,7 +16,7 @@ class GhostExecution
             $(document).ready(function () {
                 dataTable.DataTable({
                     dom: '{$dom}',
-                    lengthMenu: [ 10, 20, 30, 40, 50 ],
+                    lengthMenu: [ '{$lengthMenu}' ],
                     stateSave: true,
                     responsive:true,
                     'paging': true,
