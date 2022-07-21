fc122f22853708bb244c29c38e08b20af4ddefed
Alexandre Ferreira <shieldforce2@gmail.com>
2022-07-20 23:50:16 -0300

    adicionando js

---
+++ src/Frontend/JsRender/Ghost.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Shieldforce\Frontend\CssRender;
+
+class Ghost
+{
+    public static function head()
+    {
+        return "
+            <link href='https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.1.3/css/bootstrap.min.css' rel='stylesheet' />
+            <link href='https://cdn.datatables.net/1.12.1/css/dataTables.bootstrap5.min.css' rel='stylesheet' />
+        ";
+    }
+}
