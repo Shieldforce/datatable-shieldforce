118bf495cb883d5b6788fae922c179e5310648ef
Alexandre Ferreira <shieldforce2@gmail.com>
2022-07-21 14:25:26 -0300

    documentando e refatorando algumas classes

--- src/Helpers/ArrayOrderable.php
+++ src/Helpers/ArrayOrderable.php
@@ -2,9 +2,20 @@
 
 namespace Shieldforce\Helpers;
 
+/**
+ * Classe para ordenação de arrays
+ */
 class ArrayOrderable
 {
-    public static function array($list, $order, $dir)
+
+    /**
+     * Responsável por ordenar arrays
+     * @param $list
+     * @param $order
+     * @param $dir
+     * @return mixed
+     */
+    public static function array(array $list, string $order, string $dir) : array
     {
         $orderable = $dir == "asc" ? SORT_ASC : SORT_DESC;
         array_multisort(array_map(function ($element) use($order){
