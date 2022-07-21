e179e3316dcf79b34162cf230239d49ae6ec5e00
Alexandre Ferreira <shieldforce2@gmail.com>
2022-07-21 01:49:22 -0300

    Criando classe de retorno de dados

--- src/Frontend/CssRender/Ghost.php
+++ src/Frontend/CssRender/Ghost.php
@@ -7,9 +7,8 @@ class Ghost
     public static function head()
     {
         return "
-            <link href='https://cdn.datatables.net/1.12.1/css/jquery.dataTables.min.css' rel='stylesheet' />
-            <!--<link href='https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.1.3/css/bootstrap.min.css' rel='stylesheet' />-->
-            <!--<link href='https://cdn.datatables.net/1.12.1/css/dataTables.bootstrap5.min.css' rel='stylesheet' />-->
+            <link href='https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.1.3/css/bootstrap.min.css' rel='stylesheet' />
+            <link href='https://cdn.datatables.net/1.12.1/css/dataTables.bootstrap5.min.css' rel='stylesheet' />
             <link href='https://cdn.datatables.net/responsive/2.3.0/css/responsive.dataTables.min.css' rel='stylesheet' />
             
         ";
--- src/Frontend/JsRender/Ghost.php
+++ src/Frontend/JsRender/Ghost.php
@@ -9,10 +9,8 @@ class Ghost
         return "
             <script src='https://code.jquery.com/jquery-3.5.1.js'></script>
             <script src='https://cdn.datatables.net/1.12.1/js/jquery.dataTables.min.js'></script>
-            <!--<script src='https://cdn.datatables.net/1.12.1/js/dataTables.bootstrap.min.js'></script>-->
-            <!--<script src='https://cdn.datatables.net/fixedheader/3.2.4/js/dataTables.fixedHeader.min.js'></script>-->
-            <script src='https://cdn.datatables.net/responsive/2.3.0/js/dataTables.responsive.min.js'></script>
-            <!--<script src='https://cdn.datatables.net/responsive/2.3.0/js/responsive.bootstrap.min.js'></script>-->
+            <script src='https://cdn.datatables.net/1.12.1/js/dataTables.bootstrap5.min.js'></script>
+            <script src='https://cdn.datatables.net/responsive/2.3.0/js/responsive.bootstrap.min.js'></script>
         ";
     }
 }
