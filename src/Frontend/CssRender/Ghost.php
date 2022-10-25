e4d40da3fdf5890478cd9cef614e8cba260bd09a
Alexandre Ferreira <shieldforce2@gmail.com>
2022-10-24 21:10:43 -0300

    bootstrap 5

--- src/Frontend/CssRender/Ghost.php
+++ src/Frontend/CssRender/Ghost.php
@@ -14,9 +14,9 @@ class Ghost
     {
         if (count($links) == 0) {
             $links = [
-                "<link href='https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.1.3/css/bootstrap.min.css' rel='stylesheet' />",
+                "<link href='https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.2.0/css/bootstrap.min.css' rel='stylesheet' />",
                 "<link href='https://cdn.datatables.net/1.12.1/css/dataTables.bootstrap5.min.css' rel='stylesheet' />",
-                "<link href='https://cdn.datatables.net/responsive/2.3.0/css/responsive.dataTables.min.css' rel='stylesheet' />",
+                "<link href='https://cdn.datatables.net/responsive/2.3.0/css/responsive.bootstrap5.min.css' rel='stylesheet' />",
             ];
         }
         return implode("\n", $links);
